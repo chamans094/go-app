325cad5364a5cda0b7874bca4acb11b480bf5d74
Chaman Singh <94548103+chamans094@users.noreply.github.com>
2024-05-18 22:01:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import "fmt";
-//<<<<<<< HEAD
-// import "testing";
 
-=======
-//>>>>>>> refs/remotes/origin/main
-
-// Main function
 func main() {
 
 	fmt.Println("!... Namaste Duniya ...!")
